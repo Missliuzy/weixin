ce0538c7e699887e424154e579fc8d3c1f3d756f
zhoulin@jsb5.com <zhoulin@jsb5.com>
2019-10-08 13:34:51 +0800

    微信端

---
+++ src/components/helpBtn/helpBtn.vue
@@ -0,0 +1,37 @@
+<docs>
+  ## 帮助按钮组件
+</docs>
+<template>
+<ul>
+  <li class="return">
+    <a href="http://rsj.beijing.gov.cn/mplatform/service/list.ht">
+      <img src="../../common/images/return.png" alt="">
+    </a>
+  </li>
+</ul>
+</template>
+<script>
+export default {
+  name: 'helpBtn'
+};
+</script>
+<style scoped lang="scss">
+ul {
+  position: fixed;
+  z-index: 100;
+  right: 48px;
+  top: 8%;
+  width: 100px;
+  li {
+    list-style:none;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    float: left;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
